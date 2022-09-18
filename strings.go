e25c92a1675cccb3e6c884fdd02437acd021f7ea
Timothy <timothylam96@hotmail.com>
2022-09-19 02:54:07 +0800

    fix comment

--- strings.go
+++ strings.go
@@ -5,19 +5,23 @@ import (
 	"unsafe"
 )
 
+// Split will split the current string by the given token and return a slice of String
 func (s String) Split(sep String) []String {
 	strs := strings.Split(string(s), sep.ToString())
 	return *(*[]String)(unsafe.Pointer(&strs))
 }
 
+// ToUpper will convert all characters of the current String to uppercase
 func (s String) ToUpper() String {
 	return String(strings.ToUpper(s.ToString()))
 }
 
+// ToLower will convert all characters of the current String to lowercase
 func (s String) ToLower() String {
 	return String(strings.ToLower(s.ToString()))
 }
 
+// Trim will remove the given characters from the String instance
 func (s String) Trim(cutset String) String {
 	return String(strings.Trim(s.ToString(), cutset.ToString()))
 }
