2f5fc744618e4f127fb7898d8d71723e8eada316
Timothy <timothylam96@hotmail.com>
2022-09-18 21:25:27 +0800

    add strconv std atoi itoa benchmark

--- test/strconv_bench_test.go
+++ test/strconv_bench_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/timothyl96/goo"
@@ -14,10 +15,22 @@ func BenchmarkItoa(b *testing.B) {
 	}
 }
 
+func BenchmarkStdItoa(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = strconv.Itoa(1)
+	}
+}
+
 func BenchmarkAtoi(b *testing.B) {
 	gString := goo.FromString("1")
 
 	for i := 0; i < b.N; i++ {
 		_, _ = gString.Atoi()
+	}
+}
+
+func BenchmarkStdAtoi(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = strconv.Atoi("1")
 	}
 }
