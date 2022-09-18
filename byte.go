2f313ede1e53606a99a9367e88ca4f5edaef039b
Timothy <timothylam96@hotmail.com>
2022-09-19 02:26:16 +0800

    fix comment

--- byte.go
+++ byte.go
@@ -7,7 +7,7 @@ func FromByte(n byte) Byte {
 	return Byte(n)
 }
 
-// ToUint8 converts instance to builtin uint8
+// ToByte converts instance to builtin uint8
 func (n Byte) ToByte() byte {
 	return byte(n)
 }
