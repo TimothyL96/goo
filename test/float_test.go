84167e7a10e1dc0e20242db7ad35f63e9a741013
Timothy <timothylam96@hotmail.com>
2022-09-19 02:24:25 +0800

    fix float test coverage

--- test/float_test.go
+++ test/float_test.go
@@ -6,6 +6,25 @@ import (
 	"github.com/timothyl96/goo"
 )
 
+func TestFromFloat32(t *testing.T) {
+	want := float32(123.321)
+	get := goo.FromFloat32(want)
+
+	if get.ToFloat32() != want {
+		t.Errorf("FromFloat32(%f) = %f; want %f", want, get, want)
+	}
+}
+
+func TestToFloat32(t *testing.T) {
+	want := float32(123.321)
+	gInt := goo.Float32(want)
+	get := gInt.ToFloat32()
+
+	if get != want {
+		t.Errorf("ToFloat32() = %f; want %f", get, want)
+	}
+}
+
 func TestFromFloat64(t *testing.T) {
 	want := 123.321
 	get := goo.FromFloat64(want)
